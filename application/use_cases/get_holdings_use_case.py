0f81bb408399555a0c36e3d22531c02eb5542b21
gaurav singh <gaurav.singh@thesmartq.com>
2026-07-01 01:22:06 +0530

    str. ch.

--- application/use_cases/get_holdings_use_case.py
+++ application/use_cases/get_holdings_use_case.py
@@ -4,7 +4,7 @@ from typing import List
 
 # from domain.holding import Holding
 # from domain.repositories.holding_repository import HoldingRepository
-from infrastructure.repositories.bigquery_holdings_repository import BigQueryHoldingsRepository
+from shared.infrastructure.repositories.bigquery_holdings_repository import BigQueryHoldingsRepository
 # from domains.holdings.models import Holding
 from contexts.holdings.models import Holding
 from infrastructure.providers.kite_connect.kite_holdings_provider import KiteHoldingsProvider
